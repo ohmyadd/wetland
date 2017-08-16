ed82589ec88b01436fd10ea5a17f488fa54a8ca6
Add <ohmyadd@gmail.com>
2017-08-16 22:14:11 +0800

    p0f debug3

--- wetland/output_plugin/p0fp0f.py
+++ wetland/output_plugin/p0fp0f.py
@@ -1,22 +1,28 @@
 import os
 import p0f
-import json
 import subprocess
 from wetland import config
 
 sock = config.cfg.get("p0fp0f", "sockname")
 path = config.cfg.get("p0fp0f", "path")
+iface = config.cfg.get("p0fp0f", "iface")
 logpath = config.cfg.get("log", "path")
 
-subprocess.Popen("./p0f -d -s %s" % sock, shell=True, cwd=path,
-                 stdout=subprocess.PIPE)
+subprocess.Popen("./p0f -d -i %s -s %s" % (iface, sock), shell=True,
+                 cwd=path, stdout=subprocess.PIPE)
 
 client = p0f.P0f(os.path.join(path, sock))
 
 class plugin(object):
     def __init__(self, hacker_ip):
         self.hacker_ip = hacker_ip
+        self.sent = False
 
     def send(self, subject, action, content):
+        # log one time per transport
+        if sent:
+            return
+
         with open(os.path.join(logpath, self.hacker_ip, 'p0f.log'), 'a') as lg:
-            lg.write(json.dumps(client.get_info(self.hacker_ip)) + '\n')
+            lg.write(str(client.get_info(self.hacker_ip)) + '\n')
+        self.sent = True
