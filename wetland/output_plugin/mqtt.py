4130af9dc21052f63ab539317705c28448aa7ca7
add <ohmyadd@gmail.com>
2018-08-22 18:21:50 +0800

    wetland support for mqtt

---
+++ wetland/output_plugin/mqtt.py
@@ -0,0 +1,54 @@
+import json
+import time
+import pytz
+import datetime
+from wetland import config
+import paho.mqtt.client as mqtt
+
+
+# sensor name
+name = config.cfg.get("wetland", "name")
+
+# urls to report
+host = config.cfg.get("mqtt", "host")
+keys_path = config.cfg.get("mqtt", "key_path")
+ca_certs = keys_path + 'ca.crt'
+cert_file = keys_path + 'server.crt'
+key_file = keys_path + 'server.key'
+
+
+class plugin(object):
+    def __init__(self, server):
+        self.server = server
+
+    def send(self, subject, action, content):
+        t = datetime.datetime.fromtimestamp(time.time(),
+                                            tz=pytz.timezone('UTC')).isoformat()
+
+        if subject == 'wetland' and \
+           action in ('login_successful', 'shell command', 'exec command',
+                      'direct_request', 'reverse_request'):
+            pass
+
+        elif subject in ('sftpfile', 'sftpserver'):
+            pass
+
+        elif subject == 'content' and action in ('pwd',):
+            pass
+        else:
+            return True
+
+        data = {'timestamp': t, 'src_ip': self.server.hacker_ip,
+                'dst_ip': self.server.myip, 'action': action,
+                'content': content, 'sensor': self.name,
+                'src_port': self.server.hacker_port,
+                'dst_port': 22}
+        data = json.dumps(data) + '\n'
+
+        client = mqtt.Client()
+        client.tls_set(ca_certs=ca_certs,
+                       certfile=cert_file,
+                       keyfile=key_file)
+        client.connect(host)
+        client.publish('ck/log', data, qos=1)
+        return True
